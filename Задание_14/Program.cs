f58b7f4e69f636c6baa6bc7741a12782a865269e
vladalexgor@ya.ru <vladalexgor@ya.ru>
2021-12-06 21:04:05 +0300

    Код почищен от лишних строк

--- Задание_14/Program.cs
+++ Задание_14/Program.cs
@@ -79,39 +79,5 @@ namespace Задание_14
                 return "Гав";
             }
         }
-
-
-        /*abstract class Figure
-        {
-            public abstract double GetArea();
-            public abstract double GetPerimeter();
-            public abstract string Name { get; set; }
-        }
-
-        class Rectangular: Figure
-        {
-            string name;
-            public double Width { get; set; }
-            public double Height { get; set; }
-            public override double GetArea()
-            {
-                return Width * Height;
-            }
-            public override double GetPerimeter()
-            {
-                return (Width + Height) * 2;
-            }
-            public override string Name
-            {
-                get
-                {
-                    return name;
-                }
-                set
-                {
-                    name = value;
-                }
-            }
-        }*/
     }
 }
